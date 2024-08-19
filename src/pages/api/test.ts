7d4b05b4f58ec654c0d522c17e3a4e7c1c2fb06a
Alex Uskov <FrozenAlex@users.noreply.github.com>
2024-08-19 16:39:21 +0400

    Force to be dynamic

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,8 +1,16 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = ({params, request }) => {
+// Force to be dynamic
+export const prerender = false;
+
+export const GET: APIRoute = ({ params, request }) => {
     const current = new Date();
     return new Response(JSON.stringify({
         message: "Hello from the API! at " + current.toISOString(),
-    }))
+    }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
 }

4f7302bc45ec5145ba8c827d839ecb997540b038
Alex Uskov <FrozenAlex@users.noreply.github.com>
2024-08-19 16:31:27 +0400

    Test again

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({params, request }) => {
+    const current = new Date();
     return new Response(JSON.stringify({
-        message: "Hello from the API!",
+        message: "Hello from the API! at " + current.toISOString(),
     }))
 }
